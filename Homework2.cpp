63cf82d5893e3b3443b02910a45104c27804b200
Nori <jiwonchichi1@gmail.com>
2024-09-25 15:53:54 +0900

    240925 15:53 과제2 완료..

--- Homework2.cpp
+++ Homework2.cpp
@@ -1,3 +1,15 @@
 /*	240925 과제2
 하노이탑 : 재귀함수 사용해서 구현하기
 */
+#include<iostream>
+using namespace std;
+int Hanoi(int n) {
+	if (n <= 1)return 1;
+	return Hanoi(n - 1) * 2 + 1;
+}
+
+int main() {
+	int res = Hanoi(5);
+
+	cout << res << "\n";
+}
